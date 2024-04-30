6a9da31286f66603f84607b86adef90edcbdc9fd
MKProjects <karlo29pilares@gmail.com>
2024-04-30 12:08:59 +0800

    Assessment finished(I think)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container'
-import "./App.css";
 import SearchTextField from './Components/TextFields/search';
 import SearchButton from './Components/Buttons/search';
 import Sidebar from './Components/Sidebar/sidebar';
 import DashBoard from './Components/Pages/DashBoard';
 import Assessment from './Components/Pages/Assessment';
-import OralAssessment from './Components/Pages/OralAssessment';
-const App = () => {
+import OralAssessment from './Components/Pages/OralAssessment';const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState('DashBoard');
 
@@ -16,20 +14,20 @@ const App = () => {
   };
 
   return (
-    <>
+  <Container fluid>
     <Sidebar setCurrentPage={setCurrentPage} sendToggle={toggleSidebar} />
     <header>
       <SearchTextField/>
       <SearchButton/>
       </header>
-<Container>
+<Container fluid>
       <div className="content">
         {currentPage === 'DashBoard' && <DashBoard setCurrentPage={setCurrentPage}/>}
         {currentPage === 'Assessment' && <Assessment setCurrentPage={setCurrentPage}/>}
         {currentPage === 'OralAssessment' && <OralAssessment />}
       </div>
     </Container>
-    </>
+ </Container>
   );
 };
 
